32f1b245343d1a27e40324a8f637f44e072cb2e1
IrmuunSolo <Oyutan6677gmail.com>
2025-02-14 17:14:07 +0800

    shape interface bolon busad classiig oorchlov

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,25 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+
+// path: C:\Users\irmuu\OneDrive\Documents\2B\ПХ бүтээлт\Lab 3\f.csa311.25b-lab02\java
+// ажиллуулах код: java -cp target/lab02-1.0-SNAPSHOT.jar edu.cmu.cs.cs214.lab02.Main
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 3);
+        Shape square = new Square(4);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer circleRenderer = new Renderer(circle);
+        Renderer rectangleRenderer = new Renderer(rectangle);
+        Renderer squareRenderer = new Renderer(square);
 
-        renderer.draw();
+        circleRenderer.draw();
+        rectangleRenderer.draw();
+        squareRenderer.draw();
     }
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,17 +3,19 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
+    public Shape shape;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    Renderer(Shape shape) {
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
+        double area = shape.getArea();
+        double perimeter = shape.getPerimeter();
 
         // assume implementation
 
         System.out.println("Shape printed\n" + "Its area is " + area);
+        System.out.println("Its parameter is " + perimeter + "\n");
     }
 }
